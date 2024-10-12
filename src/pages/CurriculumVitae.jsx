f4f11a3c2a27c5f346dbebaa5f2385d363b3425e
igork <igor.polajzer@student.sum.si>
2024-10-12 15:27:49 +0200

    Removed viewing the pdf from mobile

--- src/pages/CurriculumVitae.jsx
+++ src/pages/CurriculumVitae.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './../App.css';
-import './../css/CurriculumVitae.css'
+import './../css/CurriculumVitae.css';
 import CV from './../assets/Igor_Polajzer_CV_English.pdf';
 
 function CurriculumVitae() {
+    // Regular expression to check for Android or iOS mobile devices
+    const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
     return (
         <div className="cv-section">
+            {isMobile ? (
+                <>
+                    <p>Viewing the PDF is not available on mobile yet, download the PDF instead.</p>
+                    <a href={CV} download className="pdf-download-link">
+                        Download CV
+                    </a>
+                </>
+            ) : (
                 <object data={CV} type="application/pdf" className="pdf">
                     <p>PDF failed to load</p>
-                        <p>Looks like the pdf representation failed, try to change your browser/device or just </p>
-                        <a href={CV} download className="pdf-download-link">
-                            Download the CV
-                        </a>
                 </object>
-            {/*)}*/}
+            )}
         </div>
     );
 }
