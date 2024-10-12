ed326d63c3ab6509576f2121b3d67d424c334187
igork <igor.polajzer@student.sum.si>
2024-10-12 15:19:38 +0200

    Changed the pdf representation back to object.

--- src/pages/CurriculumVitae.jsx
+++ src/pages/CurriculumVitae.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import './../App.css';
-import './../css/CurriculumVitae.css';
+import './../css/CurriculumVitae.css'
 import CV from './../assets/Igor_Polajzer_CV_English.pdf';
 
 function CurriculumVitae() {
+
     return (
         <div className="cv-section">
-            <iframe
-                src={CV}
-                title="Curriculum Vitae"
-                className="pdf"
-            >
-                <p>
-                    It appears that the PDF cannot be displayed. You can
-                    <a href={CV} download className="pdf-download-link">
-                        download the CV
-                    </a>.
-                </p>
-            </iframe>
+                <object data={CV} type="application/pdf" className="pdf">
+                    <p>PDF failed to load</p>
+                        <p>Looks like the pdf representation failed, try to change your browser/device or just </p>
+                        <a href={CV} download className="pdf-download-link">
+                            Download the CV
+                        </a>
+                </object>
+            {/*)}*/}
         </div>
     );
 }
