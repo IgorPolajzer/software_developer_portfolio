96bb5fc1059d9a9b5cf322f28b05a1be6eaed38d
igork <igor.polajzer@student.sum.si>
2025-01-27 11:41:53 +0100

    Added stars animation to the background and changed the cv screen to display via bootstrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import './../css/NavBar.css';
-import './../App.css'
-import { ReactComponent as HamburgerIcon } from './../assets/hamburger_icon.svg'
-import { ReactComponent as CloseIcon } from './../assets/close_icon.svg'
+import './../App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import { ReactComponent as HamburgerIcon } from './../assets/hamburger_icon.svg';
+import { ReactComponent as CloseIcon } from './../assets/close_icon.svg';
 
 function NavBar() {
-  const [menuOpened, setIsOpened] = useState(false); //Toggles if menu is opened or closed
+  const [menuOpened, setIsOpened] = useState(false); // Toggles if menu is opened or closed
 
   const handleMobileMenuToggle = () => {
     setIsOpened(!menuOpened);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1000) {
+        setIsOpened(false); // Close the menu if the screen width is above 1000px
+      }
+    };
+
+    // Add the resize event listener
+    window.addEventListener('resize', handleResize);
+
+    // Initial check
+    handleResize();
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-      <div className="nav-bar">
-          <div className="hamburger-icon" onClick={handleMobileMenuToggle}>
-              {menuOpened ? (
-                  <CloseIcon height={70} width={70}/>
-              ) : (
-                  <HamburgerIcon/>
-              )}
-          </div>
+      <Navbar expand="lg" className="nav-bar" fixed="top">
 
-          <div className="title">Igor Polajžer - Software developer</div>
+        <div className="title">Igor Polajžer - Software developer</div>
 
-          <ul className={menuOpened ? 'nav-links-mobile' : 'nav-links'}>
-              <Link to="/" className="nav-link">About me</Link>
-              <Link to="/projects" className="nav-link">My projects</Link>
-              <Link to="/cv" className="nav-link">CV - Curriculum Vitae</Link>
-              <Link to="/contact" className="nav-link">Contact me</Link>
-          </ul>
-      </div>
+        <ul className={menuOpened ? 'nav-links-mobile' : 'nav-links'}>
+          <Link to="/" className="nav-link">About me</Link>
+          <Link to="/projects" className="nav-link">My projects</Link>
+          <Link to="/cv" className="nav-link">CV - Curriculum Vitae</Link>
+          <Link to="/contact" className="nav-link">Contact me</Link>
+        </ul>
+
+        <div className="hamburger-icon" onClick={handleMobileMenuToggle}>
+          {menuOpened ? (
+              <CloseIcon height={70} width={70}/>
+          ) : (
+              <HamburgerIcon/>
+          )}
+        </div>
+      </Navbar>
   );
 }
 
--- src/pages/CurriculumVitae.jsx
+++ src/pages/CurriculumVitae.jsx
@@ -9,19 +9,39 @@ function CurriculumVitae() {
 
     return (
         <div className="cv-section">
-            {isMobile ? (
-                <>
-                    <br></br>
-                    <p>Viewing the PDF is not available on mobile yet, download the PDF instead.</p>
-                    <a href={CV} download className="pdf-download-link">
-                        Download CV
-                    </a>
-                </>
-            ) : (
-                <object data={CV} type="application/pdf" className="pdf">
-                    <p>PDF failed to load</p>
-                </object>
-            )}
+            <div className="container mt-4">
+                <div className="card">
+                    <div className="card-body">
+                        <h1 className="card-title">Curriculum Vitae</h1>
+                        <hr />
+                        {isMobile ? (
+                            <>
+                                <p>
+                                    Viewing the PDF is not available on mobile yet. Please download the PDF instead.
+                                </p>
+                                <a href={CV} download className="btn btn-primary">
+                                    Download CV
+                                </a>
+                            </>
+                        ) : (
+                            <div className="embed-responsive embed-responsive-16by9">
+                                <object
+                                    data={CV}
+                                    type="application/pdf"
+                                    className="embed-responsive-item"
+                                    width="100%"
+                                    height="635px"
+                                >
+                                    <p>PDF failed to load. You can download it below.</p>
+                                    <a href={CV} download className="btn btn-primary">
+                                        Download CV
+                                    </a>
+                                </object>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
