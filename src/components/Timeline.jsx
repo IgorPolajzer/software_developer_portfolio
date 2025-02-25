043420bc8843939ef657a131f5af02683f28260b
igork <igor.polajzer@student.sum.si>
2025-02-25 20:52:08 +0100

    Added timeline

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,80 @@
+import "./../css/Timeline.css"
+import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { ReactComponent as BirthdayIcon } from '../assets/icons/birthday-icon.svg'
+import { ReactComponent as OSCirkulaneZavrc } from '../assets/icons/os-cirkulane-zavrc-icon.svg'
+import { ReactComponent as SCPtujIcon } from '../assets/icons/scptuj-icon.svg'
+import { ReactComponent as FERI } from "../assets/icons/university.svg";
+import SkyLabs from "../assets/skylabs_logo.png";
+import Setronica from "../assets/setronica_logo.png"
+
+
+function Timeline(){
+    const timelineItems = [
+        {
+            date: "July 22 2003",
+            title: "Birth",
+            location: "Maribor - Slovenia",
+            icon: <BirthdayIcon />,
+        },
+        {
+            date: "September 1 2009",
+            title: "Primary School Cirkulane-Zavrč",
+            location: "Cirkulane - Slovenia",
+            icon: <OSCirkulaneZavrc />,
+        },
+                {
+            date: "September 1 2018 - 24 Junij  2022",
+            title: "Technic of mechatronics - Electrical and Computer School Ptuj",
+            location: "Ptuj - Slovenia",
+            icon: <SCPtujIcon/>
+        },
+                {
+            date: "October 1 2022 - In progress (Estimated: September 2025)",
+            title: "BSc of Computer Science and Information Technology\n" +
+                "Engineering The Faculty of Electrical Engineering and Computer",
+            location: "Maribor - Slovenia",
+            icon: <FERI/>
+        },
+                        {
+            date: "August 1 2023 - 31 September  2023",
+            title: "Software engineer(Internship) Skylabs d.o.o",
+            location: "Maribor - Slovenia",
+            icon: <img src={SkyLabs} style={{width: '85%', marginTop: 15, marginLeft: 4}} />
+        },
+                {
+            date: "October 1 2022 - In progress (Estimated: September 2025)",
+            title: "Programmer Setronica(Internship) d.o.o",
+            location: "Maribor - Slovenia",
+            icon: <img src={Setronica} style={{width: '85%', marginTop: 25, marginLeft: 5}} />
+        },
+    ]
+
+    return (
+        <>
+            <h1>Timeline</h1>
+            <div className="container">
+
+                    <VerticalTimeline>
+                        {timelineItems.map((item, index) => (
+                            <VerticalTimelineElement
+                            key={index}
+                            date={<div class={"date"}>{item.date}</div>}
+                            contentStyle={{background: '#0D1B2A66'}}
+                            contentArrowStyle={{background: "transparent", color: "#FFFFFF", marginRight:"4px"}}
+                            iconStyle={{ background: '#ddd8da'}}
+                            icon={item.icon}
+                            >
+                                <h3 class={"title"}>{item.title}</h3>
+                                <h4 class={"location"}>{item.location}</h4>
+                                <p class={"content"}>{item.content}</p>
+                            </VerticalTimelineElement>
+                        ))}
+                    </VerticalTimeline>
+            </div>
+        </>
+    )
+}
+
+export default Timeline;
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,14 +5,16 @@ import ProfileImg from "../assets/profile_picture_croped.jpg";
 import HtmlBrackets from "../assets/html_brackets_logo.png";
 import Skills from "../components/Skills";
 import Experience from "../components/Experience";
+import Timeline from "../components/Timeline";
 
 function AboutMe(){
     return(
         <div className="aboutme-section">
-            <Topic title={"Igor Polajžer"} subtitle={"SOFTWARE DEVELOPER"} text={'"The art of programming is the art of organizing complexity, of mastering multitude and avoiding its bastard chaos as effectively as possible." - Edsger Dijkstra'} image={ProfileImg}/>
-            <Topic title={"About me"} subtitle={"LIFE AND EDUCATION"} text={"I'm Igor Polajžer, a passionate 21-year-old Computer Science student at the Faculty of Electrical Engineering and Computer Science (FERI) in Maribor. Now in the final year of my studies, I am deeply committed to expanding my knowledge and honing my skills in the tech industry. With a strong work ethic and an eagerness to embrace new challenges, I am driven to continuously grow as a developer and problem-solver."} image={HtmlBrackets}/>
+            <Topic title={"Igor Polajžer"} subtitle={"SOFTWARE DEVELOPER"} text={"I'm Igor Polajžer, a passionate 21-year-old Computer Science student at the Faculty of Electrical Engineering and Computer Science (FERI) in Maribor. Now in the final year of my studies, I am deeply committed to expanding my knowledge and honing my skills in the tech industry. With a strong work ethic and an eagerness to embrace new challenges, I am driven to continuously grow as a developer and problem-solver."} image={ProfileImg}/>
             <Topic custom={<Skills/>}/>
             <Topic custom={<Experience/>}/>
+            <Timeline/>
+            {/*<Topic title={"About me"} subtitle={"LIFE AND EDUCATION"} text={"I'm Igor Polajžer, a passionate 21-year-old Computer Science student at the Faculty of Electrical Engineering and Computer Science (FERI) in Maribor. Now in the final year of my studies, I am deeply committed to expanding my knowledge and honing my skills in the tech industry. With a strong work ethic and an eagerness to embrace new challenges, I am driven to continuously grow as a developer and problem-solver."} image={HtmlBrackets}/>*/}
             <br></br>
         </div>
     );
