648c79b4c62e22563399d2baf94fb15eec46de61
Igor Polajžer <igor.polajzer@tridenstechnology.com>
2025-07-23 10:44:08 +0200

    Updated age

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -8,7 +8,7 @@ import Timeline from "../components/Timeline";
 function AboutMe(){
     return(
         <div className="aboutme-section">
-            <Topic title={"Igor Polajžer"} subtitle={"SOFTWARE DEVELOPER"} text={"I'm Igor Polajžer, a passionate 21-year-old Computer Science student at the Faculty of Electrical Engineering and Computer Science (FERI) in Maribor. Now in the final year of my studies, I am deeply committed to expanding my knowledge and honing my skills in the tech industry. With a strong work ethic and an eagerness to embrace new challenges, I am driven to continuously grow as a developer and problem-solver."} image={ProfileImg}/>
+            <Topic title={"Igor Polajžer"} subtitle={"SOFTWARE DEVELOPER"} text={"I'm Igor Polajžer, a passionate 22-year-old Computer Science student at the Faculty of Electrical Engineering and Computer Science (FERI) in Maribor. Now in the final year of my studies, I am deeply committed to expanding my knowledge and honing my skills in the tech industry. With a strong work ethic and an eagerness to embrace new challenges, I am driven to continuously grow as a developer and problem-solver."} image={ProfileImg}/>
             <Topic custom={<Skills/>}/>
             <Topic custom={<Experience/>}/>
             <Timeline/>
